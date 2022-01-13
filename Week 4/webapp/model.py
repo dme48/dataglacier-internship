dafd394c08aca986cfea3c47140d708011e86237
dme48 <dmartinezechevarria@gmail.com>
2022-01-13 15:05:38 +0100

    Added model template

---
+++ Week 4/webapp/model.py
@@ -0,0 +1,14 @@
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import pickle
+
+dataset = pd.read_csv('vgsales.csv')
+
+# Process csv
+
+# Build model, keep the object
+
+# Pickle the object
+
+# Optional: test model
