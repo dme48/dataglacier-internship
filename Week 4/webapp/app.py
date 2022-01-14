c3e32329911565403b2cec9070634f9ccbd1ee64
dme48 <dmartinezechevarria@gmail.com>
2022-01-14 10:31:56 +0100

    Refactored app.py, mainly aesthetic

--- Week 4/webapp/app.py
+++ Week 4/webapp/app.py
@@ -1,24 +1,22 @@
-import numpy as np
-from flask import Flask, request, render_template
 import pickle
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
 with open('model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
 @app.route('/')
-def home():
+def render_form():
+    """Renders the html containing the webapp form"""
     return render_template('simple_form.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
+@app.route('/predict', methods=['POST'])
+def render_prediction():
+    """Uses the user input to predict and render the result"""
     X = [float(x) for x in request.form.values()]
     prediction = round(model.predict([X])[0])
     output_text = f"The predicted age of the crab is {prediction} years."
     return render_template('simple_form.html', prediction_text=output_text)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app = Flask(__name__)
+    app.run()
